1aaec020935d6b54b229c19903c8338b5738bfaf
Sam50506 <samhere5202@gmail.com>
2026-05-19 14:02:06 +0530

    fix usdc abi

--- src/App.js
+++ src/App.js
@@ -46,7 +46,7 @@ const REMITTANCE_ABI = [
 
 const ERC20_ABI = [
   'function approve(address spender, uint256 amount) returns (bool)',
-  'function allowance(address owner, address spender) view returns (uint256)',
+  'function allowance(address owner, address spender) view returns (uint256)', 'function transfer(address to, uint256 amount) returns (bool)',
   'function balanceOf(address account) view returns (uint256)',
 ];
 
