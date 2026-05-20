cccfacc0f6ab41957cff6e874321f775853ed9cf
Sam50506 <samhere5202@gmail.com>
2026-05-20 18:45:29 +0530

    fix: pure CJS in craco.config.cjs

--- craco.config.cjs
+++ craco.config.cjs
@@ -1,17 +1,15 @@
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const webpack = require('webpack');
+const webpack = require("webpack");
 
-export default {
+module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        buffer: require.resolve('buffer/'),
-        crypto: require.resolve('crypto-browserify'),
-        stream: require.resolve('stream-browserify'),
-        assert: require.resolve('assert/'),
-        process: require.resolve('process/browser.js'),
+        buffer: require.resolve("buffer/"),
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert/"),
+        process: require.resolve("process/browser.js"),
         http: false,
         https: false,
         os: false,
@@ -21,8 +19,8 @@ export default {
       webpackConfig.plugins = [
         ...webpackConfig.plugins,
         new webpack.ProvidePlugin({
-          Buffer: ['buffer', 'Buffer'],
-          process: 'process/browser.js',
+          Buffer: ["buffer", "Buffer"],
+          process: "process/browser.js",
         }),
       ];
       return webpackConfig;
