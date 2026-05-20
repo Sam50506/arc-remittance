80d3b5635f20c06addba3b0d5cb4081ac714f155
Sam50506 <samhere5202@gmail.com>
2026-05-20 14:20:33 +0530

    fix: craco config ESM syntax for Node 24

--- craco.config.js
+++ craco.config.js
@@ -1,30 +1,30 @@
-const webpack = require("webpack");
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+const webpack = require('webpack');
 
-module.exports = {
+export default {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve.fallback = {
         ...webpackConfig.resolve.fallback,
-        buffer: require.resolve("buffer/"),
-        crypto: require.resolve("crypto-browserify"),
-        stream: require.resolve("stream-browserify"),
-        assert: require.resolve("assert/"),
-        process: require.resolve("process/browser.js"),
+        buffer: require.resolve('buffer/'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        assert: require.resolve('assert/'),
+        process: require.resolve('process/browser.js'),
         http: false,
         https: false,
         os: false,
         url: false,
         zlib: false,
       };
-
       webpackConfig.plugins = [
         ...webpackConfig.plugins,
         new webpack.ProvidePlugin({
-          Buffer: ["buffer", "Buffer"],
-          process: "process/browser.js",
+          Buffer: ['buffer', 'Buffer'],
+          process: 'process/browser.js',
         }),
       ];
-
       return webpackConfig;
     },
   },
