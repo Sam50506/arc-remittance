01d10072a895c4c489fa2de5b66956e0afd6c4f5
Sam50506 <samhere5202@gmail.com>
2026-05-21 14:12:48 +0530

    fix: add transfer to ERC20_ABI

--- src/App.js
+++ src/App.js
@@ -41,7 +41,7 @@ const REMITTANCE_ABI = [
 ];
 const ERC20_ABI = [
   'function approve(address spender, uint256 amount) returns (bool)',
-  'function allowance(address owner, address spender) view returns (uint256)',
+  'function allowance(address owner, address spender) view returns (uint256)','function transfer(address to, uint256 amount) returns (bool)',
   'function balanceOf(address owner) view returns (uint256)',
   'function decimals() view returns (uint8)'
 ];
