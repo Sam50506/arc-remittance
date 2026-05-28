a1f58914d3682904f5bd5cb2ac40fd0d077ffa24
Sam50506 <samhere5202@gmail.com>
2026-05-29 02:23:06 +0530

    fix: use token field instead of currency

--- api/faucet.js
+++ api/faucet.js
@@ -29,7 +29,7 @@ export default async function handler(req, res) {
           input: {
             destinationAddress: address,
             blockchain: 'ARC',
-            currency: 'USDC'
+            token: 'USDC'
           }
         }
       })
