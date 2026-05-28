258141d7568601b556636a55bd4fd30f513b62c9
Sam50506 <samhere5202@gmail.com>
2026-05-29 02:18:00 +0530

    fix: use blockchain field instead of chain

--- api/faucet.js
+++ api/faucet.js
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
         variables: {
           input: {
             destinationAddress: address,
-            chain: 'ARC',
+            blockchain: 'ARC',
             currency: 'USDC'
           }
         }
