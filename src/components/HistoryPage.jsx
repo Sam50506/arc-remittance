d81d7f36fe14db4580ee52e43cb9e3bec1bd91c7
Sam50506 <samhere5202@gmail.com>
2026-07-03 09:54:21 +0530

    Remove black tap highlight outline globally and on chart

--- src/components/HistoryPage.jsx
+++ src/components/HistoryPage.jsx
@@ -172,7 +172,7 @@ export default function HistoryPage({
               </select>
             </div>
           <div style={{marginTop:16}}>
-            <ResponsiveContainer width="100%" height={160}>
+            <ResponsiveContainer width="100%" height={160} style={{outline:"none"}}>
               <AreaChart data={localChartData} margin={{top:8,right:8,left:0,bottom:0}}>
                 <defs>
                   <linearGradient id="cg" x1="0" y1="0" x2="0" y2="1">
