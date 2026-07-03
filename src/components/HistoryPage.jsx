b74f4e47efa0e83ab04726569d1f641fa7f9966a
Sam50506 <samhere5202@gmail.com>
2026-07-03 10:04:43 +0530

    Remove SVG outline on recharts chart

--- src/components/HistoryPage.jsx
+++ src/components/HistoryPage.jsx
@@ -173,7 +173,7 @@ export default function HistoryPage({
             </div>
           <div style={{marginTop:16}}>
             <ResponsiveContainer width="100%" height={160} style={{outline:"none"}}>
-              <AreaChart data={localChartData} margin={{top:8,right:8,left:0,bottom:0}}>
+              <AreaChart data={localChartData} margin={{top:8,right:8,left:0,bottom:0}} style={{outline:"none"}} className="no-outline">
                 <defs>
                   <linearGradient id="cg" x1="0" y1="0" x2="0" y2="1">
                     <stop offset="5%" stopColor="#3B82C4" stopOpacity={0.2}/>
